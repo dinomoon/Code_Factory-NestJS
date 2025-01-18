a31054b7ce4d0afdd9f3fa4e9677319ac55bd1fb
MunGyung Choi <ansrud45@gmail.com>
2025-01-18 17:15:14 +0900

    Fix posts controller, service

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,65 +3,24 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '',
-    content: '',
-    likeCount: 10000,
-    commentCount: 10000,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '',
-    content: '',
-    likeCount: 10000,
-    commentCount: 10000,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '',
-    content: '',
-    likeCount: 10000,
-    commentCount: 10000,
-  },
-];
-
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPosts(): PostModel[] {
-    return posts;
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
 
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-    return post;
+    return this.postsService.getPostById(id);
   }
 
   @Post()
@@ -70,18 +29,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts.length + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts.push(post);
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   @Patch(':id')
@@ -91,36 +39,11 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    const newPost = {
-      ...post,
-      ...(author && { author }),
-      ...(title && { title }),
-      ...(content && { content }),
-    };
-    posts = posts.map((post) => {
-      if (post.id === +id) {
-        return newPost;
-      }
-
-      return post;
-    });
-
-    return newPost;
+    return this.postsService.updatePost(id, author, title, content);
   }
 
   @Delete(':id')
   deletePosts(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    posts = posts.filter((post) => post.id !== +id);
-    return id;
+    return this.postsService.deletePost(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,8 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '',
+    content: '',
+    likeCount: 10000,
+    commentCount: 10000,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '',
+    content: '',
+    likeCount: 10000,
+    commentCount: 10000,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '',
+    content: '',
+    likeCount: 10000,
+    commentCount: 10000,
+  },
+];
 
 @Injectable()
 export class PostsService {
-  getPosts(): string {
-    return 'posts';
+  getAllPosts(): PostModel[] {
+    return posts;
+  }
+
+  getPostById(id: string): PostModel {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts.length + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts.push(post);
+
+    return post;
+  }
+
+  updatePost(id: string, author: string, title: string, content: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    const newPost = {
+      ...post,
+      ...(author && { author }),
+      ...(title && { title }),
+      ...(content && { content }),
+    };
+    posts = posts.map((post) => {
+      if (post.id === +id) {
+        return newPost;
+      }
+
+      return post;
+    });
+
+    return newPost;
+  }
+
+  deletePost(id: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== +id);
+    return id;
   }
 }
